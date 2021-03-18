f7d47226f2246ffc8918bb85c70824e6b231c150
Rebecca Lacer <r.a.lacer@hotmail.com>
2021-03-18 09:26:21 -0600

    updating

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 
-var timeDisplayEl = $('#time-display');
+var dateDisplayEl = $('#date-display');
+
+function displayDate() {
+  var rightNow = moment().format('MMM DD, YYYY [-] hh:mm a');
+  dateDisplayEl.text(rightNow);
+}
+setInterval(displayDate, 1000);
+
 $(document).ready(function (){
   $(".btn").on("click", function() {
     var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
     localStorage.setItem(time, value);
-    $(".notification").addClass("show");
-
-    setTimeout(function () {
-            $(".notification").removeClass("show");
-        }, 1000);
-  } )
+     } )
 
 function hourUpdate() {
   var currentHour = moment().hours();
@@ -35,79 +37,15 @@ hourUpdate();
 var interval = setInterval(hourUpdate, 60000);
 
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 
 } )
-
-
-
-
-
-
-// var dateEl = document.getElementById("rightNow.MMM DD, YYYY")
-// var startBtnEl = document.getElementById("saveBtn");
-// var hourEl = $(hour on 24 hour clock);
-// var pastEl = $(hour < present);
-// var presentEl = $(hour = hourEl);
-// var futureEl = $(hour > hourEl);
-// var userInputEl = $(form-control (text));
-
-
-
-
-function displayTime() {
-  var rightNow = moment().format('MMM DD, YYYY [-] hh:mm a');
-  timeDisplayEl.text(rightNow);
-}
-setInterval(displayTime, 1000);
-
-// function clickuserInput() {
-//   if (this.value !== questions[quesIndex].answer) {
-//       time -= 15;
-//       if (time < 0) {
-//           time = 0;
-//       }
-//       timerEl.textContent = time;
-//       feedbackEl.textContent = "Sorry, that is incorrect.";
-
-//   } else {
-//       feedbackEl.textContent = "That is correct!";
-
-//   }
-
-//   feedbackEl.setAttribute("class", "feedback");
-//   setTimeout(function () {
-//       feedbackEl.setAttribute("class", "feedback hide");
-//   }, 1000);
-
-//   quesIndex++;
-
-//   if (quesIndex === questions.length) {
-//       quizEnd();
-//   } else {
-//       getQuestion();
-//   }
-// }
-
-// if (form-control !== "") {
-//   var userText = JSON.parse(localStorage.getItem("userInput")) || [];
-//   var newScore = {
-//       score: time,
-//       initials: initials
-//   }
-
-//  userText.push(text);
-//   localStorage.setItem("text", JSON.stringify(hiScores));
-//   location.href = "time-block";
-
-
-// }
-
-
-
-
-
-
-// submitBtn.onclick = submitText;
-
 
